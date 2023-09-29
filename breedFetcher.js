8b01495f871116efcc4529958564cb5746a311af
Alexander Da Costa <bsmuse@tutamail.com>
2023-09-29 16:36:44 -0600

    Refactor code and add index.js

---
+++ breedFetcher.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?name=${breedName}`;
+  
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback('Breed not found', null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+
+module.exports = { fetchBreedDescription }
